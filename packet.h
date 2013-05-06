b94a329352d2b115d49f618d824548dee6a03e53
Sridatta Thatipamala <sthatipamala@gmail.com>
2013-05-06 12:33:43 -0700

    packet changed

--- packet.h
+++ packet.h
@@ -4,14 +4,18 @@
 #include <time.h>
 #include <stdint.h>
 
+const int PAY_LEN = 512;
+
 typedef struct {
     uint32_t seq_number; /* 4 bytes */
     struct timeval timestamp; /* 8 bytes */
     uint32_t R; /* 4 bytes */
     uint32_t num_expected; /* 4 bytes */
+    uint32_t window_size; /* 4 bytes */
+    float timeout; /* 4 bytes */
     float avg_len; /* 4 bytes */
     char stream; /* 1 bytes */
-    char garbage[512];
+    char payload[512];
 } ee122_packet;
 
 unsigned char * serialize_packet(unsigned char * buffer, ee122_packet p) {
@@ -20,12 +24,17 @@ unsigned char * serialize_packet(unsigned char * buffer, ee122_packet p) {
     ((uint32_t*)buffer)[2] = htonl(p.timestamp.tv_usec);
     ((uint32_t*)buffer)[3] = htonl(p.R);
     ((uint32_t*)buffer)[4] = htonl(p.num_expected);
-    buffer[5*4] = ((char*)&p.avg_len)[3];
-    buffer[5*4+1] = ((char*)&p.avg_len)[2];
-    buffer[5*4+2] = ((char*)&p.avg_len)[1];
-    buffer[5*4+3] = ((char*)&p.avg_len)[0];
-    buffer[6*4] = p.stream;
-    memcpy(buffer[6*4+1], p.garbage, 512);
+    ((uint32_t*)buffer)[5] = htonl(p.window_size);
+    buffer[6*4] = ((char*)&p.timeout)[3];
+    buffer[6*4+1] = ((char*)&p.timeout)[2];
+    buffer[6*4+2] = ((char*)&p.timeout)[1];
+    buffer[6*4+3] = ((char*)&p.timeout)[0];
+    buffer[7*4] = ((char*)&p.avg_len)[3];
+    buffer[7*4+1] = ((char*)&p.avg_len)[2];
+    buffer[7*4+2] = ((char*)&p.avg_len)[1];
+    buffer[7*4+3] = ((char*)&p.avg_len)[0];
+    buffer[8*4] = p.stream;
+    memcpy(&buffer[8*4+1], p.payload, sizeof(p.payload));
 }
 
 ee122_packet deserialize_packet(unsigned char* buffer){
@@ -35,12 +44,17 @@ ee122_packet deserialize_packet(unsigned char* buffer){
   p.timestamp.tv_usec = ntohl(((uint32_t*)buffer)[2]);
   p.R = ntohl(((uint32_t*)buffer)[3]);
   p.num_expected = ntohl(((uint32_t*) buffer)[4]);
-  ((char*) &p.avg_len)[0] = buffer[5*4+3];
-  ((char*) &p.avg_len)[1] = buffer[5*4+2];
-  ((char*) &p.avg_len)[2] = buffer[5*4+1];
-  ((char*) &p.avg_len)[3] = buffer[5*4];
-  p.stream = buffer[6*4];
-  p.garbage = buffer[6*4+1];
+  p.window_size = ntohl(((uint32_t*) buffer)[5]);
+  ((char*) &p.timeout)[0] = buffer[6*4+3];
+  ((char*) &p.timeout)[1] = buffer[6*4+2];
+  ((char*) &p.timeout)[2] = buffer[6*4+1];
+  ((char*) &p.timeout)[3] = buffer[6*4];
+  ((char*) &p.avg_len)[0] = buffer[7*4+3];
+  ((char*) &p.avg_len)[1] = buffer[7*4+2];
+  ((char*) &p.avg_len)[2] = buffer[7*4+1];
+  ((char*) &p.avg_len)[3] = buffer[7*4];
+  p.stream = buffer[8*4];
+  memcpy(p.payload, &buffer[8*4+1], sizeof(p.payload));
   return p;
 }
 
